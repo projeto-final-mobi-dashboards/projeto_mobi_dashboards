8f767b7d75f76cb8c9883da02862357ffdee6dee
Gustavo Luiz <gustavoprocopio727@gmail.com>
2025-05-12 22:43:09 -0300

    add sse on changes in database, untested

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import PlotMap from "./components/mapa.jsx";
 import PlotPie from "./components/endereco.jsx";
@@ -10,6 +10,35 @@ import Sideba from "./components/sidebar/index.jsx"
 import './App.css';
 
 function App() {
+  const [Refresh,setRefresh]=useState('')
+  useEffect(
+    ()=>{
+    const eventSource= new EventSource("/refreshEvent")
+
+    eventSource.onmessage = (event)=> //evento que ocorro ao receber uma mensagem de uma fonte de evento
+    {
+    	const data = JSON.parse(event.data);
+    	setRefresh(data.update);
+    }
+
+    eventSource.onerror = (error)=> //em erro avisa no console e fecha a conexao
+    {
+		console.log("erro:", error)
+		eventSource.close();
+    }  
+
+    }
+    
+    ,[])
+  useEffect(()=>
+	{
+		if(Refresh=='True')
+		{
+			window.location.reload(); //recarrega a pagina
+		}
+	}
+  ,[Refresh])  
+
   return (
     <div className="MapDiv">
         <Sideba/>
